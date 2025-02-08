60f473b1f362be3bce0a2c84e0434fa2c35e56d7
xcorpion <umarmughal255372@gmail.com>
2025-02-08 23:51:40 +0500

    ...

--- app/api/userPosts/[id]/route.ts
+++ app/api/userPosts/[id]/route.ts
@@ -1,19 +1,16 @@
 import prisma from "@/prisma/db.config";
 import { auth } from "@/auth";
-import { NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
-interface Context {
-  params: Promise<{
-    id: string;
-  }>;
-}
-
-export const GET = async (context: Context) => {
+export const GET = async (
+  request: NextRequest,
+  context: { params: { id: string } }
+) => {
   const session = await auth();
   const user = session?.user;
   try {
-    const { id } = await context.params;
-    const post = prisma.posts.findUnique({
+    const { id } = context.params;
+    const post = await prisma.posts.findUnique({
       where: {
         id: String(id),
         userEmail: user?.email as string,
@@ -22,7 +19,7 @@ export const GET = async (context: Context) => {
 
     if (!post) {
       return NextResponse.json({
-        message: "You are not thsi post's author or invalid post Id",
+        message: "You are not this post's author or invalid post Id",
         status: 400,
       });
     }
@@ -34,5 +31,9 @@ export const GET = async (context: Context) => {
     });
   } catch (error) {
     console.error(error);
+    return NextResponse.json({
+      status: 500,
+      message: "An error occurred while fetching the post",
+    });
   }
 };
